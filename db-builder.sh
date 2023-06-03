2d421ff78fc41805b2cc619f1e93e6067a2cad73
Marwane <taroua.marwane@gmail.com>
2023-06-03 01:16:05 -0400

    initializing soccer.db

--- db-builder.sh
+++ db-builder.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-# SCRIPT TO BUILD DATABASES LOCALLY
-rm -rf ./databases/*.db  #clear old databases
+# Generating a Local Test Database 
+rm -rf ./databases/*.db  #delete old databases
 
 echo "BUILDING bball.db..."
 cat ./bball-db/bball-schemas.sql | sqlite3 ./databases/bball.db
 cat ./bball-db/bball-views.sql | sqlite3 ./databases/bball.db
 cat ./bball-db/insert-data.sql| sqlite3 ./databases/bball.db
+
+echo "BUILDING soccer.db..."
+cat ./soccer-db/soccer-schemas.sql | sqlite3 ./databases/soccer.db
+cat ./soccer-db/soccer-views.sql | sqlite3 ./databases/soccer.db
+cat ./soccer-db/insert-data.sql| sqlite3 ./databases/soccer.db
 
 echo "BUILDING users.db..."
 cat ./users-db/users-schemas.sql | sqlite3 ./databases/users.db
